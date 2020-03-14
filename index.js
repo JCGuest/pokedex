5689d4ee4ae0f178817c91af720ae8cab27c8b70
JCGuest <jcguest90@gmail.com>
2020-03-14 14:57:32 -0500

    changed form, building handleOnSerachSubmit()

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-console.log("hello")
+document.addEventListener('DOMContentLoaded', () => {
+  handleOnSearchSubmit()
+} )
+
+function handleOnSearchSubmit() {
+  const form = document.getElementById('pokemon-form')
+  form.addEventListener('submit', (e) => {
+    // const inputValue = document.getElementById('search-input').value
+    e.preventDefault()
+    console.log(e)
+  })
+}
+
+// fetch('http://api.open-notify.org/astros.json')
+// .then(function(response) {
+//   return response.json();
+// })
+// .then(function(json) {
+//   console.log(json)
+// });
